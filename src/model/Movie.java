b9f53fddf7bcceee7239653b3548489e832e485b
JFelipeFloresS <62604435+JFelipeFloresS@users.noreply.github.com>
2021-03-09 19:31:15 +0000

    dbconn

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -5,10 +5,74 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author José Felipe Flores da Silva
  */
 public class Movie {
+    
+    private final int id;
+    private final String title, category, description, restriction;
+    private boolean isAvailable;
+    private Date rentDate;
+
+    public Movie(int id, String title, String category, String description, String restriction, boolean isAvailable, Date rentDate) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.description = description;
+        this.restriction = restriction;
+        this.isAvailable = isAvailable;
+        this.rentDate = rentDate;
+    }
+
+    public Movie(int id, String title, String category, String restriction, boolean isAvailable, Date rentDate) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.restriction = restriction;
+        this.isAvailable = isAvailable;
+        this.rentDate = rentDate;
+        this.description = "Description not currently available.";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRestriction() {
+        return restriction;
+    }
+
+    public boolean isIsAvailable() {
+        return isAvailable;
+    }
+
+    public Date getRentDate() {
+        return rentDate;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public void setRentDate(Date rentDate) {
+        this.rentDate = rentDate;
+    }
+    
     
 }
