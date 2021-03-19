c94d4db2f144c4403d1210dc8d74282e3ecf815c
JFelipeFloresS <62604435+JFelipeFloresS@users.noreply.github.com>
2021-03-19 11:03:16 +0000

    connection update

--- src/model/DBConnection/Connection.java
+++ src/model/DBConnection/Connection.java
@@ -5,10 +5,38 @@
  */
 package model.DBConnection;
 
+import controller.Controller;
+import java.util.ArrayList;
+
 /**
  *
  * @author José Felipe Flores da Silva
  */
 public class Connection {
+    
+    private final String dbServer, dbUser, dbPass;
+    private Controller controller;
+    
+    public Connection (Controller controller) {
+        this.controller = controller;
+        this.dbServer = "jdbc:mysql://apontejaj.com:3306/Felipe_2019405?useSSL=false";
+        this.dbUser = "Felipe_2019405";
+        this.dbPass = "2019405";
+    }
+
+    public Connection(String dbServer, String dbUser, String dbPass) {
+        this.dbServer = dbServer;
+        this.dbUser = dbUser;
+        this.dbPass = dbPass;
+    }
+    
+    /**
+     * Sort out database first!!!!!
+     * @param id
+     * @return 
+     */
+    public static ArrayList<String> getCustomerCreditCards(int id) {
+        return new ArrayList<>();
+    }
     
 }
