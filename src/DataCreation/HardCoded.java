09f8c606cc8b71116496e5ffa54d6fbbb07e9fa9
ThyagoAlves93 <thyagoebalu93@hotmail.com>
2021-04-13 20:47:39 +0100

    Update HardCoded.java

--- src/DataCreation/HardCoded.java
+++ src/DataCreation/HardCoded.java
@@ -5,10 +5,38 @@
  */
 package DataCreation;
 
+import java.util.ArrayList;
+import model.movie.Movie;
+
 /**
  *
  * @author José Felipe Flores da Silva
  */
 public class HardCoded {
+
+    ArrayList< Movie> movies;
+
+    public HardCoded() {
+        
+        this.movies = new ArrayList<>(); 
+
+        this.movies.add(new Movie(1, "Avergers 1","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+        this.movies.add(new Movie(1, "Avergers 2","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+        this.movies.add(new Movie(1, "Avergers 3","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+        this.movies.add(new Movie(1, "Avergers 4","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+        this.movies.add(new Movie(1, "Avergers 5","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+        this.movies.add(new Movie(1, "Avergers 6","Action","-14",true,"https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg") ); 
+    }
+    public ArrayList getMovies(){ 
+                           
+        return this.movies;
+    
+    }
+    
+        
+        
+        
+        
+        
     
 }
