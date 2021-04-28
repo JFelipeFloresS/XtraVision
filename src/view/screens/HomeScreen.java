55ab84cec19087d90a0ad382b3bed4f5977b02dc
ThyagoAlves93 <thyagoebalu93@hotmail.com>
2021-04-28 20:21:35 +0100

    HomeScreamImage

--- src/view/screens/HomeScreen.java
+++ src/view/screens/HomeScreen.java
@@ -12,12 +12,18 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import model.movie.Movie;
 
 /**
@@ -29,28 +35,55 @@ public class HomeScreen extends JPanel{
     Controller controller;
     
     public HomeScreen(Controller controller) {
-        
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
         this.controller = controller;
         this.setLayout(new BorderLayout());
         
         JPanel north = new JPanel();
-        this.add(north,BorderLayout.CENTER);
+        this.add(north,BorderLayout.NORTH);
         
+        JLabel image2 = new JLabel();
+        Image img = null;
+        try {
+            img = ImageIO.read(getClass().getResource("/Images/ExtraVision.jpeg"));
+            img = img.getScaledInstance(windowSize.width,windowSize.height / 8 , java.awt.Image.SCALE_SMOOTH);
+        } catch (IOException ex) {
+            System.out.println("Image erro");
+        }
+        image2.setIcon(new ImageIcon(img));
+        north.add (image2);
+        image2.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        
+        
+         
        JPanel center = new JPanel();
        this.add(center,BorderLayout.CENTER);
+       
+       center.setLayout(new BorderLayout());
+       
+       
+       JPanel south = new JPanel();
+       this.add(south,BorderLayout.SOUTH);
+    
+        south.setPreferredSize(new Dimension(windowSize.width, windowSize.height /10));
+       
+     
    
        JLabel image = new JLabel();
-            image.setIcon(new ImageIcon(getClass().getResource("/Images/HomeScreem.jpg")));
-            center.add (image);
-            
+            image.setIcon(new ImageIcon(getClass().getResource("/Images/HomeScreem.jpeg")));
+            center.add (image,BorderLayout.CENTER);
+            image.setHorizontalAlignment(SwingConstants.CENTER);
+            JPanel buttonPanel = new JPanel();
+            center.add(buttonPanel,BorderLayout.SOUTH);
        
        
        
        
        JButton rent = new JButton("RENT");
-       center.add (rent);
+       buttonPanel.add (rent);
        JButton returnButton = new JButton("RETURN");
-       center.add (returnButton);
+       buttonPanel.add (returnButton);
        
        changeButtons(this);
        
@@ -63,20 +96,22 @@ public class HomeScreen extends JPanel{
      
     
     private void changeButtons(JPanel panel){
-        panel.setBackground(Color.white);
+        panel.setBackground(new Color(9,9,9));
         Component [] components = panel.getComponents();
         for(Component c: components){
             if (c instanceof JButton) {
                Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
-                c.setPreferredSize (new Dimension(windowSize.width /4 , windowSize.height /3));
-                c.setFont(new Font(Font.SERIF,Font.ROMAN_BASELINE,70));
-                c.setForeground(new Color(255,193,17));
-                
+                c.setPreferredSize (new Dimension(windowSize.width /6 , windowSize.height /7));
+                c.setFont(new Font(Font.SERIF,Font.BOLD,60));
+                c.setForeground(new Color(255,210,25));
+                ((JButton) c).setBorder(BorderFactory.createLineBorder(Color.WHITE,4,true));
                 
                 ((JButton) c).addActionListener(this.controller);
                 
-                c.setBackground(new Color(204,0,0));
-            }
+            ///    c.setBackground(new Color(243,242,237));
+                c.setBackground(null);
+            
+            }  
  
             if (c instanceof JPanel)
                changeButtons ((JPanel)c);
