650d26b602ad9e75f89f9e37e417546645f44106
ThyagoAlves93 <thyagoebalu93@hotmail.com>
2021-05-14 11:08:02 +0100

    almost there

--- src/view/screens/ReturnHomeScreem.java
+++ src/view/screens/ReturnHomeScreem.java
@@ -27,18 +27,18 @@ import javax.swing.SwingConstants;
  * @author thyag
  */
 public class ReturnHomeScreem extends JPanel {
-    
+
     private final Controller controller;
     private static JTextField idInput;
-    
+
     public ReturnHomeScreem(Controller controller) {
         this.controller = controller;
         this.setLayout(new BorderLayout());
-         Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
-       
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+
         JPanel north = new JPanel();
-        this.add(north,BorderLayout.NORTH);
-        
+        this.add(north, BorderLayout.NORTH);
+
         JLabel image = new JLabel();
         Image img = null;
         try {
@@ -50,81 +50,69 @@ public class ReturnHomeScreem extends JPanel {
         image.setIcon(new ImageIcon(img));
         north.add(image);
         image.setHorizontalAlignment(SwingConstants.CENTER);
-        
-         
-         
+
         JPanel center = new JPanel();
-        this.add(center,BorderLayout.CENTER);
-        
+        this.add(center, BorderLayout.CENTER);
+
         JPanel center2 = new JPanel();
         center.add(center2, BorderLayout.CENTER);
-        
+
         ReturnHomeScreem.idInput = new JTextField(20);
         center2.add(ReturnHomeScreem.idInput);
-        
+
         JPanel centerText = new JPanel();
-        center2.add(centerText,BorderLayout.CENTER);
-      
-        
+        center2.add(centerText, BorderLayout.CENTER);
+
+        JLabel label = new JLabel("Insert The DVD ID:");
+        label.setForeground(Color.WHITE);
+        center.add(label);
+        center.add(ReturnHomeScreem.idInput);
+
+        JPanel east = new JPanel();
+        this.add(east, BorderLayout.EAST);
+
         JButton returnDvd = new JButton("Return DVD");
-        center.add(returnDvd,BorderLayout.WEST);
-        
+        east.add(returnDvd, BorderLayout.EAST);
+
         returnDvd.setCursor(new Cursor(Cursor.HAND_CURSOR));
         returnDvd.setActionCommand("return DVD");
         returnDvd.addActionListener(this.controller);
         returnDvd.setPreferredSize(new Dimension(windowSize.width / 7, windowSize.height / 8));
         returnDvd.setBackground(new Color(186, 199, 202));
         returnDvd.setForeground(new Color(9, 9, 9));
-        returnDvd.setFont(new Font(Font.SERIF, Font.BOLD, 25));;
-        
-        
-        
-        JLabel label = new JLabel("Insert The DVD ID:");
-        label.setForeground(Color.WHITE);
-        
-         
-     
-        
+        returnDvd.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+
+        JPanel west = new JPanel();
+        this.add(west, BorderLayout.WEST);
+
         JButton homeS = new JButton("Home Screen");
-        center.add(homeS,BorderLayout.EAST);
-        
+        west.add(homeS, BorderLayout.WEST);
+
         homeS.setCursor(new Cursor(Cursor.HAND_CURSOR));
         homeS.setActionCommand("Go to rent home screem");
-        homeS.addActionListener(this.controller);        
+        homeS.addActionListener(this.controller);
         homeS.setPreferredSize(new Dimension(windowSize.width / 7, windowSize.height / 8));
         homeS.setBackground(new Color(186, 199, 202));
         homeS.setForeground(new Color(9, 9, 9));
-        homeS.setFont(new Font(Font.SERIF, Font.BOLD, 25));;
-        
-        
-        
-        center.add(homeS,BorderLayout.EAST);
-        center.add(label);
-        center.add(ReturnHomeScreem.idInput);
-        center.add(returnDvd,BorderLayout.WEST);
-        
-        
+        homeS.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+
         JPanel south = new JPanel();
-        this.add(south,BorderLayout.SOUTH);
-        
+        this.add(south, BorderLayout.SOUTH);
+
         JButton dvdR = new JButton("return DVD using card");
-        south.add(dvdR,BorderLayout.CENTER);
+        south.add(dvdR, BorderLayout.CENTER);
         dvdR.setCursor(new Cursor(Cursor.HAND_CURSOR));
         dvdR.setActionCommand("return DVD using card");
-        dvdR.addActionListener(this.controller);        
+        dvdR.addActionListener(this.controller);
         dvdR.setPreferredSize(new Dimension(windowSize.width / 7, windowSize.height / 8));
         dvdR.setBackground(new Color(186, 199, 202));
         dvdR.setForeground(new Color(9, 9, 9));
         dvdR.setFont(new Font(Font.SERIF, Font.BOLD, 20));;
-        
-        
-        
-        
-        
+
     }
-    
+
     public static String getReturnIDInput() {
         return ReturnHomeScreem.idInput.getText();
     }
-    
+
 }
